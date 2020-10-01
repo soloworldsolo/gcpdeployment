1f35aceadb37f48802c38ea28adfdf3036753680
solomon <shaminsolomonit@gmail.com>
2020-10-01 22:37:06 +0530

    dcdc

---
+++ src/main/java/com/soloworld/poc/nonnullpoc/GcpDeployment.java
@@ -0,0 +1,13 @@
+package com.soloworld.poc.nonnullpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcpDeployment {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcpDeployment.class, args);
+	}
+
+}
